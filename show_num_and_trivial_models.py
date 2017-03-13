67a9f5825e8a03c31c067632081639d62050f381
Dylan Hamilton <dahamilt@ucsd.edu>
2017-03-13 04:36:19 -0700

    Updated value and category + show num and trivial

--- show_num_and_trivial_models.py
+++ show_num_and_trivial_models.py
@@ -10,11 +10,8 @@ from nltk.corpus import stopwords
 from sklearn.svm import LinearSVC
 import sys
 
-
 stopwords = set(stopwords.words("english"))
 punctuation = set(string.punctuation)
-print("starting")
-sys.stdout.flush()
 def parseData():
     with open('jeopardy.json') as data_file:
         data = data_file.read()
@@ -22,21 +19,17 @@ def parseData():
         return eval(data)
 
 data = parseData()
-#print parseData()[:2]
-print("DATA PARSED")
-sys.stdout.flush()
 
 #gets total word counts
 def wordCount(data):
     word_count = defaultdict(int)
     for d in data:
-        words = ''.join([c for c in d['question'].lower() + d['answer'].lower() if not c in punctuation]).split()
+        words = ''.join([c for c in d['question'].lower() if not c in punctuation]).split()
         for w in words: # and if w not in stopwords:
             word_count[w] += 1
     word_count = [(word_count[w], w) for w in word_count]
     word_count.sort()
     word_count.reverse()
-    #print("total unique words: " + str(len(word_count)))
     return word_count[:1000]   # return last-however-many here
 
 #gets total category counts
@@ -46,13 +39,9 @@ def categoryCount(data):
     wordFreqCat = {}
     for d in data:
         category_counts[d['category']] += 1
-
     category_counts = [(category_counts[k], k) for k in category_counts]
     category_counts.sort()
     category_counts.reverse()
-    #print "Number of categories: {}\nTop 10 categories: {}".format(len(category_counts), category_counts[:10])
-    #print "Mean number of items per categories: {}\nStd dev of items per categories: {}".format(np.mean([x[0] for x in category_counts]), np.std([x[0] for x in category_counts]))
-    #print("total unique categories: " + str(len(category_counts)))
     return category_counts[:50]
 
 #gets total value counts
@@ -67,8 +56,6 @@ def valueCount(data):
     value_counts = [(value_counts[k], k) for k in value_counts]
     value_counts.sort()
     value_counts.reverse()
-    #print "Number of categories: {}\nTop 10 categories: {}".format(len(category_counts), category_counts[:10])
-    #print "Mean number of items per categories: {}\nStd dev of items per categories: {}".format(np.mean([x[0] for x in category_counts]), np.std([x[0] for x in category_counts]))
     return value_counts
 
 word_counts = wordCount(data)
@@ -77,10 +64,6 @@ value_counts = valueCount(data)
 word_id = {x[1]: num for (x, num) in zip(word_counts, range(len(word_counts)))}
 category_id = {x[1]: num for (x, num) in zip(category_counts, range(len(category_counts)))}
 value_id = {x[1]: num for (x, num) in zip(value_counts, range(len(value_counts)))}
-
-# for key in value_counts:
-#     print("ValCounts, Val: " + str(key))
-
 
 #word frequency per category for kategory prediction
 wordFreq = defaultdict(float)
@@ -92,7 +75,7 @@ punctuation = string.punctuation
 #category word count totals
 for d in data:
     if d['category'] in category_id.keys():
-        words = ''.join([c for c in d['question'].lower() + d['answer'].lower() if not c in punctuation]).split()
+        words = ''.join([c for c in d['question'].lower() if not c in punctuation]).split()
         for w in words: # and if w not in stopwords:
             wordFreq[w] += 1.0 # Word frequency
             wordFreqCat[category_id[d['category']]][w] += 1.0 # Per-category frequency
@@ -122,8 +105,6 @@ commonWords = [x[1] for x in topWords[:1000]]
 commonWordsPositions = dict(zip(commonWords, range(len(commonWords))))
 commonWordsSet = set(commonWords)
 
-
-
 def feature(r):
   feat = [0] * len(commonWords)
   rText = ''.join([c for c in (r['question'].lower() + r['answer'].lower()) if not c in punctuation])
@@ -131,8 +112,6 @@ def feature(r):
     if w in commonWordsSet:
       feat[commonWordsPositions[w]] = 1
   return feat
-
-
 
 print ("feature vector for word count: " + str(len(wordCount(data))))
 print ("total possible value count: " + str(len(valueCount(data))))
@@ -149,11 +128,7 @@ for d in data:
         string = "".join([x for x in d['value'] if x not in ['$',',']])
         val = float(string)
         if val <= 1000 and val%100==0:
-            x = [0] * len(word_id)
-            words = ''.join([c for c in d['question'].lower() if not c in punctuation]).split()
-            for w in words:
-                if w in word_id.keys():
-                    x[word_id[w]] = 1
+            x = [1, float(d['show_number'])]
             X.append(x)
             if val in value_id.keys():
                 y.append(value_id[val])
@@ -168,9 +143,9 @@ X_train, X_val = np.asarray(X[:(len(X)/2)]), np.asarray(X[(len(X)/2):])
 y_train, y_val = np.asarray(y[:(len(y)/2)]), np.asarray(y[(len(y)/2):])
 
 #these train and validate for category prediction based on word and answer text
-X_train2 = [feature(r) for r in data[:len(data)/2] if r['category'] in category_id.keys()]
+X_train2 = [[1,float(r['show_number'])] for r in data[:len(data)/2] if r['category'] in category_id.keys()]
 y_train2 = [category_id[r['category']] for r in data[:len(data)/2] if r['category'] in category_id.keys()]
-X_val2 = [feature(r) for r in data[len(data)/2:] if r['category'] in category_id.keys()]
+X_val2 = [[1,float(r['show_number'])] for r in data[len(data)/2:] if r['category'] in category_id.keys()]
 y_val2 = [category_id[r['category']] for r in data[len(data)/2:]  if r['category'] in category_id.keys()]
 
 print("length category prediction train set: " + str(len(y_train2)))
@@ -179,20 +154,18 @@ sys.stdout.flush()
 
 model = LinearSVC(C=1)
 model.fit(X_train, y_train)
-# predictions = [int(x) for x in model.predict(X_val)]
-# acc = [(x == y) for (x,y) in zip(predictions, y_val)]
-# acc = sum(acc) * 1.0 / len(acc)
-
-print "Score of value prediction: {}".format(model.score(X_val, y_val))
-
+predictions = [value_id[value_counts[0][1]]] * len(X_val)#predictions is just most common value
+acc = [(x == y) for (x,y) in zip(predictions, y_val)]
+acc = sum(acc) * 1.0 / len(acc)
+print "Score value prediction: {}".format(model.score(X_val, y_val))
+print ("Trivial value prediction: " + str(acc))
 sys.stdout.flush()
 
 model2 = LinearSVC(C=1)
 model2.fit(X_train2, y_train2)
-# predictions2 = [int(x) for x in model.predict(X_val2)]
-# acc2 = [(x == y) for (x,y) in zip(predictions2, y_val2)]
-# acc2 = sum(acc2) * 1.0 / len(acc2)
-
-print "Score of category prediction: {}".format(model.score(X_val2, y_val2))
-
+predictions2 = [category_id[category_counts[0][1]]]*len(X_val2)#predictions most common category
+acc2 = [(x == y) for (x,y) in zip(predictions2, y_val2)]
+acc2 = sum(acc2) * 1.0 / len(acc2)
+print "Score category prediction: {}".format(model.score(X_val2, y_val2))
+print ("Trivial category prediction: " + str(acc2))
 sys.stdout.flush()
